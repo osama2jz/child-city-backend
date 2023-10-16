1acf3b4fae7ce9afa23cccafde6f69c70882e1be
Muhammad Usama <mosama4u@gmail.com>
2023-10-16 16:44:14 +0500

    coupen code fix

--- routes/route.coupen.js
+++ routes/route.coupen.js
@@ -14,6 +14,7 @@ router.put("/:id", editCoupen);
 router.get("/", viewAllCoupens);
 router.delete("/:id", deleteCoupen);
 router.post("/:code/:phone", checkCoupen);
+router.post("/:code", checkCoupen);
 router.post("/changeStatus/:id", statusChange);
 
 export default router;
